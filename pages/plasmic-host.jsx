8bec6093ae5ef3b22369882e2139bbc9b8e4e079
ClemensJakob <wackenag@gmail.com>
2022-03-11 12:08:03 +0100

    registered component

--- pages/plasmic-host.jsx
+++ pages/plasmic-host.jsx
@@ -3,6 +3,17 @@ import * as React from 'react';
 import Script from 'next/script';
 import { PlasmicCanvasHost } from '@plasmicapp/loader-nextjs';
 import { PLASMIC } from '../plasmic-init';
+import WordRotator from '../components/WordRotator';
+
+import { registerComponent } from '@plasmicapp/host';
+
+registerComponent(WordRotator, {
+  name: "WordRotatorComp",
+  props: {
+    words: 'words'
+  },
+  importPath: './components/WordRotator',
+})
 
 export default function PlasmicHost() {
   return PLASMIC && (
